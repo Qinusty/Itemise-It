9e8b55b12f0960194559018b56335d93e08ca7b2
qinusty <qinusty@gmail.com>
2015-08-14 21:59:08 +0100

    Added bootstrap and started the design. Also started the database access layer (DAL).

---
+++ Itemise-It/DAL/ChampionListContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using RiotApi.Net.RestClient.Dto.LolStaticData.Champion;
+using RiotApi.Net.RestClient.Dto.LolStaticData.Generic;
+namespace Itemise_It.DAL
+{
+    public class ChampionListContext : DbContext
+    {
+        public DbSet<ChampionListDto> ChampionList { get; set; }
+        public DbSet<ChampionDto> Champion { get; set; }
+        public DbSet<ChampionSpellDto> ChampionSpell { get; set; }
+        public DbSet<PassiveDto> ChampionPassive { get; set; }
+
+        public ChampionListContext() : base("ChampionListContext")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
