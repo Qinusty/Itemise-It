3fb6614634c3f3c489b81a4843cdbea2e2ac966e
qinusty <qinusty@gmail.com>
2015-08-14 22:03:32 +0100

    added HomeController

---
+++ Itemise-It/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RiotApi.Net.RestClient;
+using RiotApi.Net.RestClient.Configuration;
+using System.Data.;
+namespace Itemise_It.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
