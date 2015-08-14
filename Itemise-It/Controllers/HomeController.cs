e1ea1b9f344752699f03c6330c1b74c2e2dd927a
qinusty <qinusty@gmail.com>
2015-08-14 22:15:36 +0100

    Fix error cs1001, identifier expected.

--- Itemise-It/Controllers/HomeController.cs
+++ Itemise-It/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using RiotApi.Net.RestClient;
 using RiotApi.Net.RestClient.Configuration;
-using System.Data.;
+using System.Data;
 namespace Itemise_It.Controllers
 {
     public class HomeController : Controller
