ee7746424dda3f4ba4db452173b261070ad2287a
qinusty <qinusty@gmail.com>
2015-08-21 23:12:09 +0100

    Missing Files

---
+++ RiotApiDAL/Contexts/PatchContext.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Entity;
+using RiotApi.Net.RestClient.Dto.LolStaticData.Champion;
+using System.Configuration;
+using RiotApiDAL.Static;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Linq;
+using System;
+using RiotApiDAL.Static.Champion;
+using System.Collections.Generic;
+using RiotApiDAL.Contexts;
+
+namespace RiotApiDAL.Contexts
+{
+    public class PatchContext : DbContext
+    {
+        public DbSet<Patch> PatchList { get; set; }
+
+        public PatchContext() : base(ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString)
+        {
+            this.Configuration.LazyLoadingEnabled = true;
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+        }
+        public static bool PatchExists(string version)
+        {
+            if (GetPatch(version) != null) return true;
+            else return false;
+        }
+        public static Patch GetPatch(string version)
+        {
+            var context = new PatchContext();
+            var retVal = context.PatchList.Where(p => p.version == version).FirstOrDefault();
+            if (retVal == null)
+            {
+                return null;
+            }
+            else return retVal;
+        }
+        
+    }
+}
---
+++ UpdateItemise-It/Emailer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+namespace UpdateItemise_It
+{
+    public class Emailer
+    {
+        public Emailer()
+        {
+            var fromAddress = new MailAddress("jrsmith9822@gmail.com", "From Name");
+            var toAddress = new MailAddress("to@example.com", "To Name");
+            const string fromPassword = "fromPassword";
+            const string subject = "Subject";
+            const string body = "Body";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+        
+    }
+}
